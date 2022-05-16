f2be9ff51b694df56cca85319f0f57ba6262734d
4yvakZ <35529078+4yvakZ@users.noreply.github.com>
2022-05-16 14:09:07 +0300

    All done
    Couse complete

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -34,7 +34,7 @@ public class MainManager : MonoBehaviour
         }
     }
 
-    public static MainManager Instance;
+    public static MainManager Instance { get; private set; }
     public Color TeamColor;
     private void Awake()
     {
